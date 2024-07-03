1c0cc2083e31ff8eed5dc6a908c8bdb80816f244
Venugopal K <venu.shastri@gmail.com>
2024-07-03 15:19:49 +0530

    Update StringCalculatorTest.java

--- src/test/TddPracticeInJava/StringCalculatorTest.java
+++ src/test/TddPracticeInJava/StringCalculatorTest.java
@@ -1,1 +1,17 @@
+package CodeTestCoverJava;
 
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class StringCalculatorTest {
+
+    @Test
+    public void testEmptyString() {
+       //  assertTrue(Soundex.generateSoundex("")=="");
+    }
+
+    @Test
+    public void testSingleCharacter() {
+         //assertTrue(Soundex.generateSoundex("A")=="A000");
+    }
+}
